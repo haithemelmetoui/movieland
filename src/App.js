c85349d165c98902f0fa9d6b4f805be0e047840e
haithemelmetoui <elmetoui.haithem@gmail.com>
2024-11-03 01:29:31 +0100

    Add movies to the web site using omdb APIs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
 import './App.css';
+import SearchIcon from './search.svg'
+import MovieCard from "./MovieCard";
+//4bb07115
+
+const API_URL = 'http://www.omdbapi.com?apikey=4bb07115';
 
 const App = () => {
-  return (
-    <div className="App">
-      <p>Hello</p>
-    </div>
-  );
+
+    const [searchTerm, setSearchTerm] = useState("");
+    const [movies, setMovies] = useState([]);
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search);
+    }
+    useEffect(() => {
+        searchMovies("spiderman");
+    }, [])
+    return (
+        <div className="app">
+            <h1>MovieLand</h1>
+            <div className="search">
+                <input 
+                placeholder="Search for movies"
+                value={searchTerm}
+                onChange={(e) => {
+                    setSearchTerm(e.target.value)
+                }}
+                />
+                <img src={SearchIcon}
+                alt="search"
+                onClick={()=>searchMovies(searchTerm)}/>
+            </div>
+            {movies?.length > 0 ? (
+                <div className="container">
+                {movies.map((movie) => (
+                    <MovieCard movie={movie} />
+                ))}
+                </div>
+            ) : (
+                <div className="empty">
+                <h2>No movies found</h2>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
